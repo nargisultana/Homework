b7ed5bdaa51fc1a8f9a19e52dd559a45f6a027c5
nargisultana <nrgissltn@gmail.com>
2022-10-28 22:25:31 -0400

    updated hw

--- src/hw4JavaVariables/AboutMETest.java
+++ src/hw4JavaVariables/AboutMETest.java
@@ -2,52 +2,38 @@ package hw4JavaVariables;
 
 public class AboutMETest {
 	public static void main(String[] args) {
-	AboutMe aboutMe1 = new AboutMe ();
-	
-//	constructor is initialized here
-//	variables are initialized 
-	
-	aboutMe1. name = " Nargis Sultana";
-	aboutMe1. age = 66;
-	aboutMe1. myMonthlySalary = 3899;
-	aboutMe1. myYearlySalary = 130827784;
-	aboutMe1. myWeight = 1.567889f;
-	aboutMe1. myHeight = 3.600678532567;
-	aboutMe1. myApartmentRent = 77549961200675566l;
-	aboutMe1. sex = 'F';
-	aboutMe1. myParmanentResidence = true;
-	
-	aboutMe1. aboutMe();
-	
-	System.out.println("\n----------------------------------------------\n");
-	AboutMe aboutMeAlex = new AboutMe ();
-	//Variables are initialized
-	aboutMeAlex. name = "Alex";
-	aboutMeAlex. age = 63;
-	aboutMeAlex. myMonthlySalary = 3500;
-	aboutMeAlex. myYearlySalary = 140778467;
-	aboutMeAlex. myWeight = 1.676234f;
-	aboutMeAlex. myHeight = 3.75366446643663;
-	aboutMeAlex. myApartmentRent = 8545533561300956654l;
-	aboutMeAlex. sex = 'M';
-	aboutMeAlex. myParmanentResidence = true;
-	
-	aboutMeAlex.aboutMe();
-	
-	
-			
-	
-	
+		AboutMe aboutMe1 = new AboutMe();
 
-	
-	
-	
-		
-		
-		
-		
-		
-		
+		// constructor is initialized here
+		// variables are initialized
+
+		aboutMe1.name = " Nargis Sultana";
+		aboutMe1.age = 66;
+		aboutMe1.myMonthlySalary = 3899;
+		aboutMe1.myYearlySalary = 130827784;
+		aboutMe1.myWeight = 1.567889f;
+		aboutMe1.myHeight = 3.600678532567;
+		aboutMe1.myApartmentRent = 77549961200675566l;
+		aboutMe1.sex = 'F';
+		aboutMe1.myParmanentResidence = true;
+
+		aboutMe1.aboutMe();
+
+		System.out.println("\n----------------------------------------------\n");
+		AboutMe aboutMeAlex = new AboutMe();
+		// Variables are initialized
+		aboutMeAlex.name = "Alex";
+		aboutMeAlex.age = 63;
+		aboutMeAlex.myMonthlySalary = 3500;
+		aboutMeAlex.myYearlySalary = 140778467;
+		aboutMeAlex.myWeight = 1.676234f;
+		aboutMeAlex.myHeight = 3.75366446643663;
+		aboutMeAlex.myApartmentRent = 8545533561300956654l;
+		aboutMeAlex.sex = 'M';
+		aboutMeAlex.myParmanentResidence = true;
+
+		aboutMeAlex.aboutMe();
+
 	}
 
-} 
+}
--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -2,7 +2,7 @@ package hw4JavaVariables;
 
 class AboutMe {
 	// variable Declared.
-	
+
 	public String name;
 	public byte age;
 	public short myMonthlySalary;
@@ -11,10 +11,9 @@ class AboutMe {
 	public double myHeight;
 	public long myApartmentRent;
 	public char sex;
-	public boolean myParmanentResidence; 
-	
+	public boolean myParmanentResidence;
 
- 	// This is constructor
+	// This is constructor
 	// Here constructor is Declared
 	public AboutMe() {
 		System.out.println("This is all about us; \n");
@@ -26,13 +25,10 @@ class AboutMe {
 
 		System.out.println("My Name: " + name + "\nMy Age: " + age + "\nMy Monthly Salary: " + myMonthlySalary
 				+ "\nMy Yearly Salary: " + myYearlySalary + "\nMy Weight: " + myWeight + "\nMy Height: " + myHeight
-				+ "\nMy Apartment Rent: " + myApartmentRent + "\nMy Gender: " + sex + "\nAm I Parmanent Residence? Ans: "
-				+ myParmanentResidence);}
-		
+				+ "\nMy Apartment Rent: " + myApartmentRent + "\nMy Gender: " + sex
+				+ "\nAm I Parmanent Residence? Ans: " + myParmanentResidence);
+	}
 
-		
-	
-		
-		}
+}
 	
 		
