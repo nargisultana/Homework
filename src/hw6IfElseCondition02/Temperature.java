099fcc0e7ded8dbf5b2bea8519cbc17c191b0454
nargisultana <nrgissltn@gmail.com>
2022-11-13 16:32:50 -0500

    hw6IfElseCondition02

---
+++ src/hw6IfElseCondition02/Temperature.java
@@ -0,0 +1,26 @@
+package hw6IfElseCondition02;
+
+import java.util.Scanner;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		System.out.println("Todays weather");
+		Scanner scanner = new Scanner(System.in);
+		int temperature = scanner.nextInt();
+		if (temperature < 32) {
+			System.out.println("Freezing");
+		} else if (temperature < 55) {
+			System.out.println("Pleasant");
+		} else if (temperature < 73) {
+			System.out.println("Getting Warmer");
+		} else if (temperature > 101) {
+			System.out.println("Hot");
+		} else {
+			System.out.println("Please put ice on my Head");
+		}
+		scanner.close();
+
+	}
+
+}
